476155f094fd3ec1271c80f9a318efa12ce58476
PogosTorosian <pogosstudio@gmail.com>
2026-09-19 16:10:59 +0400

    chore: set up ESLint

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+﻿import js from '@eslint/js';
+import {defineConfig} from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+
+export default defineConfig([
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+
+  {
+    linterOptions: {
+      noInlineConfig: true,
+    },
+  },
+
+  {
+    files: ['**/*.{js,ts}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strict,
+      tseslint.configs.stylistic,
+      'unicorn/recommended'],
+
+    plugins: {
+      unicorn: eslintPluginUnicorn,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },]);
