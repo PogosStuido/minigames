277a5dd9ecee9cbaa30cb0bb1d1949b706c250fc
PogosTorosian <pogosstudio@gmail.com>
2026-09-20 23:11:51 +0400

    feat: implement layout of header component

---
+++ src/app/index.ts
@@ -0,0 +1,14 @@
+﻿import { renderHomePage } from '../pages/home/home-page';
+
+function boostrap(): void {
+  const appElement = document.createElement('div');
+  appElement.id = 'app';
+
+  const homePage = renderHomePage();
+
+  appElement.append(homePage);
+
+  document.body.append(appElement);
+}
+
+document.addEventListener('DOMContentLoaded', boostrap);
---
+++ src/pages/home/home-page.ts
@@ -0,0 +1,16 @@
+﻿import '../../style.scss';
+import '../../components/header/header.scss';
+import { renderHeader } from '../../components/header/header';
+
+export function renderHomePage(): HTMLElement {
+  const body = document.querySelector('body');
+  const mainElement = document.createElement('main');
+  mainElement.className = 'home-page';
+
+  const header = renderHeader();
+
+  body?.append(header);
+  mainElement.append();
+
+  return mainElement;
+}
