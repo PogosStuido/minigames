056702ee290be3cde5d5211df385986e3d3ffdd1
PogosTorosian <pogosstudio@gmail.com>
2026-09-24 18:33:30 +0400

    feat: implement hero section 1920px

---
+++ src/components/hero/hero.ts
@@ -0,0 +1,19 @@
+﻿import './hero.scss';
+
+export function renderHero(): HTMLElement {
+  const heroElement = document.createElement('section');
+  heroElement.className = 'hero';
+  heroElement.innerHTML = `
+    <div class="container">
+      <div class="hero__wrapper">
+        <h1 class="hero__title_h1">Take a Short Break & Have Fun</h1>
+        <p class="hero__text">
+          Discover hundreds of curated casual mini-games. Play instantly in your
+          browser — puzzle, match 3, farm, and board classics.
+        </p>
+        <button class="hero__button" type="button">Browse Library</button>
+      </div>
+    </div>
+  `;
+  return heroElement;
+}
--- src/pages/home/home-page.ts
+++ src/pages/home/home-page.ts
@@ -1,6 +1,7 @@
 ﻿import '../../style.scss';
 import '../../components/header/header.scss';
 import { renderHeader } from '../../components/header/header';
+import { renderHero } from '../../components/hero/hero.ts';
 
 export function renderHomePage(): HTMLElement {
   const body = document.querySelector('body');
@@ -8,9 +9,9 @@ export function renderHomePage(): HTMLElement {
   mainElement.className = 'home-page';
 
   const header = renderHeader();
-
+  const hero = renderHero();
+  mainElement.append(hero);
   body?.append(header);
-  mainElement.append();
 
   return mainElement;
 }
